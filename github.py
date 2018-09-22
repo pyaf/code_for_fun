7f7bb1cf09615b63eb45afbf0ab4791b604d7501
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2018-09-22 23:06:46 +0530

    Cleaned

--- github.py
+++ github.py
@@ -1,13 +1,13 @@
+# script to check for available github usernames
+
 import requests
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 all_usernames = [x for x in alphabet]
 
 for i, username in enumerate(all_usernames):
-	res = requests.get('https://github.com/'+username)
-	if res.status_code == 404:
-		print(username)
-	if not i%100:
-		print(i)
-
-
+    res = requests.get('https://github.com/'+username)
+    if res.status_code == 404:
+        print(username)
+    if not i % 100:
+        print(i)
